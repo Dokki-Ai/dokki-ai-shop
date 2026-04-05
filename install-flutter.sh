cb2123b18e43dbd73b3e80ec6fc887ea2bf2a0fe
ap <app@dokki.org>
2026-04-06 01:47:15 +0500

    build: add build_runner to deployment script

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-git clone https://github.com/flutter/flutter.git -b stable
+
+# 1. Скачиваем Flutter SDK (если папки еще нет)
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+
+# 2. Добавляем в PATH для текущей сессии
 export PATH="$PATH:`pwd`/flutter/bin"
-flutter config --enable-web
-flutter build web --release
+
+# 3. Настройка и получение зависимостей
+./flutter/bin/flutter config --enable-web
+./flutter/bin/flutter pub get
+
+# 4. ГЕНЕРАЦИЯ КОДА (Riverpod, Drift, JSON Serializable)
+# Это создаст все необходимые .g.dart файлы перед сборкой
+./flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs
+
+# 5. ФИНАЛЬНАЯ СБОРКА
+./flutter/bin/flutter build web --release
